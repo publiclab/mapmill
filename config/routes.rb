47ed39192ce608262fd9566b6faa9b0072fbcd4f
jywarren <jeff@unterbahn.com>
2011-04-02 01:04:05 -0400

    site-specific sorting

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
+  map.connect 'site/:id', :controller => 'miller', :action => 'site'
 
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
