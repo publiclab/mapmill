5bd47c73abc6fc6c9aa472972194003f57e59b20
Jeffrey Warren <jeff@unterbahn.com>
2011-07-06 00:49:37 +0300

    ajax voting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function vote(id,vote,site) {
+	// this is not working: "Uncaught SyntaxError: Unexpected token )"
+	new Ajax.Request("/vote/"+id+"/"+vote+"/"+site+"?ajax=true", {
+		method: "get",
+//		parameters: {},
+		onComplete: function(response) {
+			$('buttons-'+id).addClassName('grey');
+			$$('#buttons-'+id+' a').each(function(el) { el.onclick = ""; });
+		}
+	})
+}
