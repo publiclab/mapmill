1c5aad1432fea848a8b77b44228077d2ba898032
Fabio Barone <holistic.computing@gmail.com>
2014-07-24 06:08:07 -0500

    completely deleted branch to start anew

--- app/helpers/miller_helper.rb
+++
@@ -1,2 +0,0 @@
-module MillerHelper
-end
--- db/migrate/20110808165645_add_map_export.rb
+++
@@ -1,9 +0,0 @@
-class AddMapExport < ActiveRecord::Migration
-  def self.up
-    add_column :sites, :map_export, :string, :default => ""
-  end
-
-  def self.down
-    remove_column :sites, :map_export
-  end
-end
